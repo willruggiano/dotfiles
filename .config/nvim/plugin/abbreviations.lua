874ac98eeb6e388a0a31a27e842289266c7a5257
Will Ruggiano <wmruggiano@gmail.com>
2021-10-04 18:58:47 -0600

    Sync from dotfiles-old

--- .config/nvim/plugin/abbreviations.lua
+++ .config/nvim/plugin/abbreviations.lua
@@ -1,3 +1,4 @@
+vim.cmd [[cnoreabbrev h vert h]]
 vim.cmd [[iabbrev <expr> NOW   strftime("%Y-%m-%d %R")]]
 vim.cmd [[iabbrev <expr> TODAY strftime("%Y-%m-%d")]]
 vim.cmd [[iabbrev <expr> USER  $USER]]
--- .config/nvim/plugin/commands.lua
+++ .config/nvim/plugin/commands.lua
@@ -1,10 +1,8 @@
-local cmd = vim.cmd
+vim.cmd [[command! PackerInstall packadd packer.nvim | lua R('bombadil.plugins').install()]]
+vim.cmd [[command! PackerUpdate packadd packer.nvim | lua R('bombadil.plugins').update()]]
+vim.cmd [[command! PackerSync packadd packer.nvim | lua R('bombadil.plugins').sync()]]
+vim.cmd [[command! PackerClean packadd packer.nvim | lua R('bombadil.plugins').clean()]]
+vim.cmd [[command! PackerCompile packadd packer.nvim | lua R('bombadil.plugins').compile()]]
+vim.cmd [[command! PackerStatus packadd packer.nvim | lua R('bombadil.plugins').status()]]
 
-cmd [[command! PackerInstall packadd packer.nvim | lua R('bombadil.plugins').install()]]
-cmd [[command! PackerUpdate packadd packer.nvim | lua R('bombadil.plugins').update()]]
-cmd [[command! PackerSync packadd packer.nvim | lua R('bombadil.plugins').sync()]]
-cmd [[command! PackerClean packadd packer.nvim | lua R('bombadil.plugins').clean()]]
-cmd [[command! PackerCompile packadd packer.nvim | lua R('bombadil.plugins').compile()]]
-cmd [[command! PackerStatus packadd packer.nvim | lua R('bombadil.plugins').status()]]
-
-cmd [[cnoreabbrev h vert h]]
+vim.cmd [[command! BOnly lua require("close_buffers").delete({ type = "hidden" })]]
