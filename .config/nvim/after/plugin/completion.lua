2189a79ad91bd6b44d4a9296b496a6e7234fae1c
Will Ruggiano <wmruggiano@gmail.com>
2021-10-10 23:52:22 -0600

    Remove disabled cmp setup

--- .config/nvim/after/plugin/completion.lua
+++ .config/nvim/after/plugin/completion.lua
@@ -6,67 +6,21 @@ vim.opt.completeopt = { "menuone", "noselect" }
 vim.opt.shortmess:append "c"
 vim.opt.pumheight = 20
 
-local use_cmp = false
-local use_coq = true
+require "coq_3p" {
+  {
+    src = "nvimlua",
+    short_name = "nLUA",
+    conf_only = false,
+  },
+  {
+    src = "repl",
+    sh = "zsh",
+    -- shell = { p = "perl", n = "node", ... },
+    max_lines = 99,
+    deadline = 500,
+  },
+}
 
-local setup_cmp = function()
-  local cmp = require "cmp"
-  cmp.setup {
-    snippet = {
-      expand = function(args)
-        require("luasnip").lsp_expand(args.body)
-      end,
-    },
-
-    mapping = {
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-u>"] = cmp.mapping.scroll_docs(4),
-      ["<C-e>"] = cmp.mapping.close(),
-      ["<C-y>"] = cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-
-      -- TODO: Not sure I'm in love with this one.
-      ["<C-Space>"] = cmp.mapping.complete(),
-    },
-
-    sources = {
-      { name = "buffer" },
-      { name = "path" },
-      { name = "nvim_lua" },
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-    },
-  }
-
-  require("nvim-autopairs.completion.cmp").setup {
-    map_cr = true,
-    map_complete = true,
-    auto_select = false,
-  }
-end
-
-local setup_coq = function()
-  require "coq_3p" {
-    {
-      src = "nvimlua",
-      short_name = "nLUA",
-    },
-    {
-      src = "repl",
-      sh = "zsh",
-      -- shell = { p = "perl", n = "node", ... },
-      max_lines = 99,
-      deadline = 500,
-    },
-  }
-
-  inoremap { "<c-h>", "<c-\\><c-n><cmd>lua COQ.Nav_mark()<cr>" }
-end
-
-if use_cmp then
-  setup_cmp()
-elseif use_coq then
-  setup_coq()
-end
+-- Explicitly disabled in vim.g.coq_settings and mapped here instead to avoid the normal mode
+-- mapping of the same key which conflicts with a custom mapping in keymaps.lua
+inoremap { "<c-h>", "<c-\\><c-n><cmd>lua COQ.Nav_mark()<cr>" }
