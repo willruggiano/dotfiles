b0cb9176c8a1bcbd66cf3764327ce9e5f875e8da
Will Ruggiano <wmruggiano@gmail.com>
2021-11-09 08:48:00 -0700

    fix(nvim): dotfiles

--- .config/nvim/lua/telescope/_extensions/dotfiles.lua
+++ .config/nvim/lua/telescope/_extensions/dotfiles.lua
@@ -3,33 +3,13 @@ if not has_telescope then
   error "This plugin requires telescope.nvim"
 end
 
-local finders = require "telescope.finders"
-local make_entry = require "telescope.make_entry"
-local pickers = require "telescope.pickers"
-local sorters = require "telescope.sorters"
-
-local Job = require "plenary.job"
-
 local dotfiles = function(opts)
   assert(vim.env.DOTFILES, "$DOTFILES must be set")
   opts = opts or {}
-  pickers.new(opts, {
+  require("telescope.builtin").find_files(vim.tbl_deep_extend("keep", opts, {
     prompt_title = "~ dotfiles ~",
-    finder = finders._new {
-      fn_command = function(_, prompt)
-        return {
-          writer = Job:new {
-            command = "rg",
-            args = { "--hidden", "--files", vim.env.DOTFILES },
-          },
-          command = "fzf",
-          args = { "--filter", prompt },
-        }
-      end,
-      entry_maker = make_entry.gen_from_file(opts),
-    },
-    sorter = sorters.get_generic_fuzzy_sorter(),
-  }):find()
+    search_dirs = { vim.env.DOTFILES, vim.env.DOTFILES .. "/.config" },
+  }))
 end
 
 return telescope.register_extension {
