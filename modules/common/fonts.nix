5c6487cad2d76a2da033fce1136306f649daf606
Will Ruggiano <wruggian@amazon.com>
2021-12-06 16:36:07 -0700

    fix(darwin): use legacy enableFontDir

--- modules/common/fonts.nix
+++ modules/common/fonts.nix
@@ -4,7 +4,8 @@ with lib;
 
 {
   config = {
-    fonts.fontDir.enable = true;
+    # NOTE: nix-darwin does not have the fontDir option. So sticking with the legacy one for now.
+    fonts.enableFontDir = true;
     fonts.fonts = with pkgs; [
       font-awesome
     ];
