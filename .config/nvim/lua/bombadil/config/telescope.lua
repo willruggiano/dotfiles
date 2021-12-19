c6add62775849b03469683656ec2f65507d1414f
Will Ruggiano <wmruggiano@gmail.com>
2021-12-19 02:10:28 -0700

    refactor(nvim): cleanup a bunch of unused telescope crap

--- .config/nvim/lua/bombadil/config/telescope.lua
+++ .config/nvim/lua/bombadil/config/telescope.lua
@@ -1,51 +1,11 @@
-local map_tele = require "bombadil.telescope.mappings"
-local sorters = require "telescope.sorters"
-
-map_tele("<space>gw", "grep_string", {
-  short_path = true,
-  word_match = "-w",
-  only_sort_text = true,
-  layout_strategy = "vertical",
-  sorter = sorters.get_fzy_sorter(),
-})
-map_tele("<space>f/", "grep_last_search", {
-  layout_strategy = "vertical",
-})
-
--- Files
-map_tele("<space>ft", "git_files")
-map_tele("<space>fg", "live_grep")
-map_tele("<space>fo", "oldfiles")
-map_tele("<space>fd", "fd")
-map_tele("<space>pp", "project_search")
-map_tele("<space>fv", "find_nvim_source")
-map_tele("<space>fe", "file_browser")
-map_tele("<space>fz", "search_only_certain_files")
-
--- Git
-map_tele("<space>gs", "git_status")
-map_tele("<space>gc", "git_commits")
-
--- Nvim
-map_tele("<space>fb", "buffers")
-map_tele("<space>fa", "installed_plugins")
-map_tele("<space>fi", "search_all_files")
-map_tele("<space>ff", "curbuf")
-map_tele("<space>fh", "help_tags")
-map_tele("<space>vo", "vim_options")
-map_tele("<space>gp", "grep_prompt")
-
--- Telescope Meta
-map_tele("<space>fB", "builtin")
-
 require("which-key").register {
-  ["<space>ed"] = {
+  ["<space>d"] = {
     function()
       require("telescope").extensions.dotfiles.dotfiles()
     end,
     "dotfiles",
   },
-  ["<space>fp"] = {
+  ["<space>p"] = {
     function()
       require("telescope").extensions.project.project {}
     end,
--- .config/nvim/lua/bombadil/telescope/init.lua
+++ .config/nvim/lua/bombadil/telescope/init.lua
@@ -32,12 +32,22 @@ end
 
 local _ = pcall(require, "nvim-nonicons")
 
---[[ local project_dirs = {}
-for i, dir in ipairs { "~/.config", "~/dev", "~/dotfiles", "~/workspaces", "~/src" } do
+local common_dirs = {
+  ["~/.config"] = "conf",
+  ["~/dev"] = "dev",
+  ["~/dotfiles"] = "dot",
+  ["~/notes"] = "notes",
+  ["~/src"] = "src",
+  ["~/workspaces"] = "work",
+}
+local frecency_workspaces = {}
+local project_dirs = {}
+for dir, abbr in pairs(common_dirs) do
   if vim.fn.isdirectory(dir) then
+    frecency_workspaces[abbr] = vim.fn.expand(dir)
     table.insert(project_dirs, dir)
   end
-end ]]
+end
 
 telescope.setup {
   defaults = {
@@ -112,13 +122,7 @@ telescope.setup {
   extensions = {
     frecency = {
       show_scores = true,
-      workspaces = {
-        ["dev"] = vim.fn.expand "~/dev",
-        ["dot"] = vim.fn.expand "~/dotfiles",
-        ["conf"] = vim.fn.expand "~/.config",
-        ["src"] = vim.fn.expand "~/src",
-        ["work"] = vim.fn.expand "~/workspaces",
-      },
+      workspaces = frecency_workspaces,
     },
 
     fzf = {
@@ -129,12 +133,7 @@ telescope.setup {
     },
 
     project = {
-      base_dirs = {
-        "~/.config",
-        "~/dev",
-        "~/src",
-        "~/workspaces",
-      },
+      base_dirs = project_dirs,
     },
 
     ["ui-select"] = {
@@ -158,350 +157,3 @@ end
 if vim.fn.executable "gh" == 1 then
   pcall(require("telescope").load_extension, "gh")
 end
-
-local custom_actions = {}
-
-custom_actions.delete_buffer = function(prompt_bufnr)
-  local current_picker = action_state.get_current_picker(prompt_bufnr)
-  current_picker:delete_selection(function(selection)
-    -- avoid preview win from closing by creating tmp buffer
-    local preview_win = state.get_status(prompt_bufnr).preview_win
-    if preview_win ~= nil and vim.api.nvim_win_is_valid(preview_win) then
-      local buf = vim.api.nvim_create_buf(false, true)
-      vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-      vim.api.nvim_win_set_buf(preview_win, buf)
-    end
-    delete_buffer(selection.bufnr)
-  end)
-end
-
-local M = {}
-
-function M.edit_neovim()
-  local opts_with_preview, opts_without_preview
-
-  opts_with_preview = {
-    prompt_title = "~ dotfiles ~",
-    cwd = "~/dotfiles/nvim",
-
-    layout_strategy = "flex",
-    layout_config = {
-      width = 0.9,
-      height = 0.8,
-
-      horizontal = {
-        width = { padding = 0.15 },
-      },
-      vertical = {
-        preview_height = 0.75,
-      },
-    },
-
-    attach_mappings = function(_, map)
-      map("i", "<c-y>", set_prompt_to_entry_value)
-      map("i", "<M-c>", function(prompt_bufnr)
-        actions.close(prompt_bufnr)
-        vim.schedule(function()
-          require("telescope.builtin").find_files(opts_without_preview)
-        end)
-      end)
-
-      return true
-    end,
-  }
-
-  opts_without_preview = vim.deepcopy(opts_with_preview)
-  opts_without_preview.previewer = false
-
-  require("telescope.builtin").find_files(opts_with_preview)
-end
-
-function M.edit_dotfiles()
-  local opts_with_preview, opts_without_preview
-
-  opts_with_preview = {
-    prompt_title = "~ dotfiles ~",
-    cwd = "~/dotfiles",
-
-    layout_strategy = "flex",
-    layout_config = {
-      width = 0.9,
-      height = 0.8,
-
-      horizontal = {
-        width = { padding = 0.15 },
-      },
-      vertical = {
-        preview_height = 0.75,
-      },
-    },
-
-    attach_mappings = function(_, map)
-      map("i", "<c-y>", set_prompt_to_entry_value)
-      map("i", "<M-c>", function(prompt_bufnr)
-        actions.close(prompt_bufnr)
-        vim.schedule(function()
-          require("telescope.builtin").find_files(opts_without_preview)
-        end)
-      end)
-
-      return true
-    end,
-  }
-
-  opts_without_preview = vim.deepcopy(opts_with_preview)
-  opts_without_preview.previewer = false
-
-  require("telescope.builtin").find_files(opts_with_preview)
-end
-
-function M.find_nvim_source()
-  require("telescope.builtin").find_files {
-    prompt_title = "~ nvim ~",
-    cwd = "~/src/neovim/",
-
-    layout_strategy = "horizontal",
-    layout_config = {
-      preview_width = 0.35,
-    },
-  }
-end
-
-function M.edit_zsh()
-  require("telescope.builtin").find_files {
-    cwd = "~/dotfiles/zsh/",
-    prompt = "~ dotfiles ~",
-
-    layout_strategy = "horizontal",
-    layout_config = {
-      preview_width = 0.55,
-    },
-  }
-end
-
-function M.fd()
-  require("telescope.builtin").fd()
-end
-
-function M.builtin()
-  require("telescope.builtin").builtin()
-end
-
-function M.git_files()
-  local path = vim.fn.expand "%:h"
-
-  local opts = themes.get_ivy {
-    winblend = 5,
-
-    cwd = path,
-
-    layout_config = {
-      width = 0.25,
-    },
-  }
-
-  require("telescope.builtin").git_files(opts)
-end
-
-function M.buffer_git_files()
-  require("telescope.builtin").git_files(themes.get_dropdown {
-    cwd = vim.fn.expand "%:p:h",
-    winblend = 10,
-    border = true,
-    previewer = false,
-  })
-end
-
-function M.lsp_code_actions()
-  local opts = themes.get_dropdown {
-    winblend = 10,
-    border = true,
-    previewer = false,
-  }
-
-  require("telescope.builtin").lsp_code_actions(opts)
-end
-
-function M.live_grep()
-  require("telescope").extensions.fzf_writer.staged_grep {
-    path_display = { "shorten" },
-    previewer = false,
-    fzf_separator = "|>",
-  }
-end
-
-function M.grep_prompt()
-  require("telescope.builtin").grep_string {
-    path_display = { "shorten" },
-    search = vim.fn.input "Grep String > ",
-  }
-end
-
-function M.grep_last_search(opts)
-  opts = opts or {}
-
-  local register = vim.fn.getreg("/"):gsub("\\<", ""):gsub("\\>", ""):gsub("\\C", "")
-
-  opts.path_display = { "shorten" }
-  opts.word_match = "-w"
-  opts.search = register
-
-  require("telescope.builtin").grep_string(opts)
-end
-
-function M.oldfiles()
-  require("telescope").extensions.frecency.frecency()
-end
-
-function M.my_plugins()
-  require("telescope.builtin").find_files {
-    cwd = "~/plugins/",
-  }
-end
-
-function M.installed_plugins()
-  require("telescope.builtin").find_files {
-    cwd = vim.fn.stdpath "data" .. "/site/pack/packer/start/",
-  }
-end
-
-function M.project_search()
-  local opts = themes.get_ivy {
-    cwd = require("lspconfig.util").root_pattern ".git"(vim.fn.expand "%:p"),
-  }
-  require("telescope.builtin").find_files(opts)
-end
-
-function M.buffers()
-  local opts = themes.get_ivy {
-    attach_mappings = function(_, map)
-      map("i", "<c-d>", custom_actions.delete_buffer)
-      map("n", "<c-d>", custom_actions.delete_buffer)
-      return true
-    end,
-  }
-  require("telescope.builtin").buffers(opts)
-end
-
-function M.curbuf()
-  local opts = themes.get_dropdown {
-    winblend = 10,
-    border = true,
-    previewer = false,
-  }
-  require("telescope.builtin").current_buffer_fuzzy_find(opts)
-end
-
-function M.help_tags()
-  require("telescope.builtin").help_tags {
-    show_version = true,
-  }
-end
-
-function M.search_all_files()
-  require("telescope.builtin").find_files {
-    find_command = { "rg", "--no-ignore", "--files" },
-  }
-end
-
-function M.example_for_prime()
-  -- local Sorter = require('telescope.sorters')
-
-  -- require('telescope.builtin').find_files {
-  --   sorter = Sorter:new {
-  -- }
-end
-
-function M.file_browser()
-  local opts
-
-  opts = {
-    sorting_strategy = "ascending",
-    scroll_strategy = "cycle",
-    layout_config = {
-      prompt_position = "top",
-    },
-
-    attach_mappings = function(prompt_bufnr, map)
-      local current_picker = action_state.get_current_picker(prompt_bufnr)
-
-      local modify_cwd = function(new_cwd)
-        current_picker.cwd = new_cwd
-        current_picker:refresh(opts.new_finder(new_cwd), { reset_prompt = true })
-      end
-
-      map("i", "-", function()
-        modify_cwd(current_picker.cwd .. "/..")
-      end)
-
-      map("i", "~", function()
-        modify_cwd(vim.fn.expand "~")
-      end)
-
-      local modify_depth = function(mod)
-        return function()
-          opts.depth = opts.depth + mod
-
-          local current_picker = action_state.get_current_picker(prompt_bufnr)
-          current_picker:refresh(opts.new_finder(current_picker.cwd), { reset_prompt = true })
-        end
-      end
-
-      map("i", "<M-=>", modify_depth(1))
-      map("i", "<M-+>", modify_depth(-1))
-
-      map("n", "yy", function()
-        local entry = action_state.get_selected_entry()
-        vim.fn.setreg("+", entry.value)
-      end)
-
-      return true
-    end,
-  }
-
-  require("telescope.builtin").file_browser(opts)
-end
-
-function M.git_status()
-  local opts = themes.get_dropdown {
-    winblend = 10,
-    border = true,
-    previewer = false,
-  }
-
-  -- Can change the git icons using this.
-  -- opts.git_icons = {
-  --   changed = "M"
-  -- }
-
-  require("telescope.builtin").git_status(opts)
-end
-
-function M.git_commits()
-  require("telescope.builtin").git_commits {
-    winblend = 5,
-  }
-end
-
-function M.search_only_certain_files()
-  require("telescope.builtin").find_files {
-    find_command = {
-      "rg",
-      "--files",
-      "--type",
-      vim.fn.input "Type: ",
-    },
-  }
-end
-
-return setmetatable({}, {
-  __index = function(_, k)
-    reloader()
-
-    if M[k] then
-      return M[k]
-    else
-      return require("telescope.builtin")[k]
-    end
-  end,
-})
