51d7aa144148ae8e92ede01a0194dab5d11c6daa
Will Ruggiano <wruggian@amazon.com>
2022-01-05 12:41:12 -0700

    fix(nvim): prefer cmake-format to lsp format

--- .config/nvim/lua/bombadil/config/lsp.lua
+++ .config/nvim/lua/bombadil/config/lsp.lua
@@ -262,7 +262,11 @@ lspconfig.clangd.setup {
 
 lspconfig.cmake.setup {
   on_init = on_init,
-  on_attach = on_attach,
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+  end,
   capabilities = updated_capabilities,
 }
 
