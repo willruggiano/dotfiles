a2199cd76b241bceeac0fef2a57ee05bbf8bf2e6
Will Ruggiano <wmruggiano@gmail.com>
2022-02-01 09:31:32 -0700

    fix(nvim): jumps

--- .config/nvim/lua/bombadil/lib/jump.lua
+++ .config/nvim/lua/bombadil/lib/jump.lua
@@ -1,6 +1,12 @@
 return function(direction)
   local count = vim.v.count
   local bound = vim.g.jump_mark_bound or 5
+
+  if count == 0 then
+    vim.cmd("normal! g" .. direction)
+    return
+  end
+
   if count > bound then
     vim.cmd [[normal! m']]
   end
