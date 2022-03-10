842adafa2ce48b8f915d35a14de3ead8653ceae9
Will Ruggiano <wruggian@amazon.com>
2022-03-09 18:52:25 -0700

    feat(nvim): prefer single line comments for cpp

---
+++ .config/nvim/after/ftplugin/cpp.lua
@@ -0,0 +1,5 @@
+local kommentary = require "kommentary.config"
+
+kommentary.configure_language("cpp", {
+  prefer_single_line_comments = true,
+})
--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -3,34 +3,18 @@ vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
 vim.bo.textwidth = 100
 
-local ok, _ = pcall(require, "luadev")
-if not ok then
-  return
-end
+require "luadev"
 
-local ok, wk = pcall(require, "which-key")
-if not ok then
-  return
-end
-
-local ok, kommentary = pcall(require, "kommentary.config")
-if not ok then
-  return
-end
+local kommentary = require "kommentary.config"
 
 kommentary.configure_language("lua", {
   prefer_single_line_comments = true,
 })
 
-wk.register({
-  ["<leader><leader>r"] = {
-    name = "run",
-    l = { "<Plug>(Luadev-RunLine)", "line" },
-    o = { "<Plug>(Luadev-Run)", "operator" },
-    w = { "<Plug>(Luadev-RunWord)", "word" },
-  },
-}, { buffer = 0 })
+local nnoremap = require("bombadil.lib.keymap").nnoremap
+local vnoremap = require("bombadil.lib.keymap").vnoremap
 
-wk.register({
-  ["<leader><leader>r"] = { "<Plug>(Luadev-Run)", "run" },
-}, { buffer = 0, mode = "v" })
+nnoremap("<leader><leader>rl", "<Plug>(Luadev-RunLine)", { buffer = 0 })
+nnoremap("<leader><leader>ro", "<Plug>(Luadev-Run)", { buffer = 0 })
+nnoremap("<leader><leader>rw", "<Plug>(Luadev-RunWord)", { buffer = 0 })
+vnoremap("<leader><leader>r", "<Plug>(Luadev-Run)", { buffer = 0 })
