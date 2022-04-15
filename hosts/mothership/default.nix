1c6fc0131171d229a7389a1f5092c38fed619fd1
Will Ruggiano <wmruggiano@gmail.com>
2022-04-15 14:08:17 -0600

    feat(nix): obs studio

--- hosts/mothership/default.nix
+++ hosts/mothership/default.nix
@@ -17,32 +17,24 @@ with lib; {
   user.shell = pkgs.zsh;
   sshPublicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERAQpJ3mjcz+b2Y+Wf598wURIrGU710Sr91HCcwSiXS bombadil@mothership";
 
-  # Hardware
-  hardware.pulseaudio.enable = true;
   hardware.bluetooth.enable = true;
+  hardware.pulseaudio.enable = true;
 
-  # Gaming
-  programs.steam.enable = true;
-
-  # Web browsing
-  programs.qutebrowser.enable = true;
-  programs.qutebrowser.default = true;
-  programs.firefox.enable = false; # via home.nix
-
-  # Desktop
   programs.discord.enable = true;
-  programs.libreoffice.enable = true;
-  programs.spotify.enable = true;
-  programs.sway.enable = true;
-
-  # Terminal
   programs.docsets.enable = true;
+  programs.firefox.enable = false; # via home.nix
   programs.kitty.enable = true;
+  programs.libreoffice.enable = true;
   programs.neovim.enable = true;
+  programs.obs-studio.enable = true;
+  programs.qutebrowser.default = true;
+  programs.qutebrowser.enable = true;
+  programs.spotify.enable = true;
+  programs.steam.enable = true;
+  programs.sway.enable = true;
   programs.taskwarrior.enable = true;
   programs.zk.enable = true;
 
-  # Services
   services.agenix.enable = true;
   services.dunst.enable = true;
   services.email.enable = true;
@@ -52,7 +44,6 @@ with lib; {
   services.pcscd.enable = true;
   services.udev.packages = [pkgs.yubikey-personalization];
 
-  # Virtualization
   virtualisation.docker.enable = true;
-  virtualisation.virtualbox.host.enable = false;
+  # virtualisation.virtualbox.host.enable = true;
 }
---
+++ modules/nixos/obs.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.programs.obs-studio;
+in {
+  options.programs.obs-studio = {
+    enable = mkEnableOption "Enable obs-studio";
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = let
+      obs = pkgs.wrapOBS {
+        plugins = with pkgs.obs-studio-plugins; [wlrobs];
+      };
+    in [obs];
+  };
+}
