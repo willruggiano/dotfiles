eb418b38a40c42c6108596792b2f8a91571cb241
Will Ruggiano <wmruggiano@gmail.com>
2022-05-08 23:59:23 -0600

    feat(ecthelion): fix invisible cursor

--- hosts/ecthelion/default.nix
+++ hosts/ecthelion/default.nix
@@ -23,16 +23,21 @@
   programs.docsets.enable = true;
   programs.kitty.enable = true;
   programs.neovim.enable = true;
-  programs.qutebrowser.enable = true;
-  programs.qutebrowser.default = true;
+  programs.qutebrowser = {
+    enable = true;
+    default = true;
+  };
   programs.spotify.enable = true;
-  programs.sway.enable = true;
+  programs.sway = {
+    enable = true;
+    wlr.hardware-cursors = false;
+  };
   programs.zk.enable = true;
 
   services.agenix.enable = true;
   services.dunst.enable = true;
   # services.email.enable = true;
-  # services.expressvpn.enable = true;
+  services.expressvpn.enable = true;
   services.interception-tools.enable = true;
   services.pcscd.enable = true;
   services.udev.packages = [pkgs.yubikey-personalization];
--- modules/nixos/sway/default.nix
+++ modules/nixos/sway/default.nix
@@ -7,37 +7,46 @@
 with lib; let
   cfg = config.programs.sway;
 in {
-  config = mkIf cfg.enable {
-    programs.sway = {
-      extraPackages = with pkgs; [
-        dmenu-wayland
-        swayidle
-        swaylock
-        wl-clipboard
-      ];
-
-      wrapperFeatures.gtk = true;
-    };
-
-    environment.loginShellInit = ''
-      [[ "$(tty)" == /dev/tty1 ]] && exec sway
-    '';
-
-    programs.waybar.enable = true;
-
-    programs.light.enable = true;
-    user.extraGroups = ["video"];
-
-    hardware.opengl.enable = true;
-    hardware.opengl.driSupport = true;
-
-    home.configFile."sway/config".text = import ./sway.nix {inherit pkgs;};
-    home.configFile."waybar/config".text = import ./waybar.nix {inherit pkgs;};
-    home.configFile = {
-      waybar = {
-        source = ../../../.config/waybar;
-        recursive = true;
-      };
-    };
+  options.programs.sway.wlr = {
+    hardware-cursors = mkEnableOption "wlr hardware cursor support";
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      programs.sway = {
+        extraPackages = with pkgs; [
+          dmenu-wayland
+          swayidle
+          swaylock
+          wl-clipboard
+        ];
+
+        wrapperFeatures.gtk = true;
+      };
+
+      environment.loginShellInit = ''
+        [[ "$(tty)" == /dev/tty1 ]] && exec sway
+      '';
+
+      programs.waybar.enable = true;
+
+      programs.light.enable = true;
+      user.extraGroups = ["video"];
+
+      hardware.opengl.enable = true;
+      hardware.opengl.driSupport = true;
+
+      home.configFile."sway/config".text = import ./sway.nix {inherit pkgs;};
+      home.configFile."waybar/config".text = import ./waybar.nix {inherit pkgs;};
+      home.configFile = {
+        waybar = {
+          source = ../../../.config/waybar;
+          recursive = true;
+        };
+      };
+    }
+    (mkIf (!cfg.wlr.hardware-cursors) {
+      environment.variables.WLR_NO_HARDWARE_CURSORS = "1";
+    })
+  ]);
 }
