5e632aa29a31a44a8c757cb6f5a9d8e90d57dec0
Will Ruggiano <wmruggiano@gmail.com>
2022-05-24 01:00:59 -0600

    feat(awesome): balrog

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -91,7 +91,7 @@ theme.titlebar_maximized_button_focus_inactive = themes_path .. "default/titleba
 theme.titlebar_maximized_button_normal_active = themes_path .. "default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active = themes_path .. "default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path .. "default/background.png"
+theme.wallpaper = gfs.get_configuration_dir() .. "wallpaper/balrog.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path .. "default/layouts/fairhw.png"
