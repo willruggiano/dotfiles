85cf11f203d243b2bddd427eaade6491bb679f97
Will Ruggiano <wmruggiano@gmail.com>
2022-07-28 23:18:03 -0600

    fix(spotify): wanted-by, backend

--- modules/nixos/spotify.nix
+++ modules/nixos/spotify.nix
@@ -7,12 +7,18 @@
 with lib; let
   cfg = config.programs.spotify;
   spotifyd = pkgs.spotifyd.override {withPulseAudio = true;};
-  spotifydConf = pkgs.writeText "spotifyd.conf" ''
-    [global]
-    username = "1211559862"
-    password_cmd = "cat ${config.age.secrets.spotify.path}"
-    backend = "pulseaudio"
-  '';
+  spotifydConf = let
+    backend =
+      if config.hardware.pulseaudio.enable
+      then "pulseaudio"
+      else "alsa";
+  in
+    pkgs.writeText "spotifyd.conf" ''
+      [global]
+      username = "1211559862"
+      password_cmd = "cat ${config.age.secrets.spotify.path}"
+      backend = "${backend}"
+    '';
 in {
   options.programs.spotify = {
     enable = mkEnableOption "Enable spotify";
@@ -23,7 +29,7 @@ in {
 
     systemd.user.services.spotifyd = {
       description = "spotifyd, a Spotify playing daemon";
-      wantedBy = ["multi-user.target"];
+      wantedBy = ["default.target"];
       environment.SHELL = "/bin/sh";
       serviceConfig = {
         ExecStart = "${spotifyd}/bin/spotifyd --no-daemon --cache-path ${config.user.home}/.cache/spotifyd --config-path ${spotifydConf}";
