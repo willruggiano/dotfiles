5caa781612de6933fb656a2c26ab7b6130710cbf
Will Ruggiano <wmruggiano@gmail.com>
2022-10-07 13:18:24 -0600

    feat(awesome): keybindings

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -75,9 +75,7 @@ local modkey = "Mod1"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
   awful.layout.suit.tile,
-  awful.layout.suit.tile.left,
-  awful.layout.suit.spiral.dwindle,
-  awful.layout.suit.magnifier,
+  awful.layout.suit.max,
 }
 -- }}}
 
