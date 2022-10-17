553bca9b19397dd8342220366b6860350132193a
Will Ruggiano <wmruggiano@gmail.com>
2022-10-17 16:52:02 -0600

    fix(colorctl): bat

--- modules/common/colorctl.nix
+++ modules/common/colorctl.nix
@@ -27,36 +27,7 @@ in {
     };
   };
 
-  config = mkIf cfg.enable (
-    mkMerge [
-      {
-        user.packages = with pkgs; [colorctl];
-      }
-      (mkIf pkgs.stdenv.isLinux {
-        systemd.user.services =
-          mapAttrs' (
-            name: cfg:
-              nameValuePair "apply-${name}-theme" (mkIf cfg.enable {
-                description = "Re-apply ${name} theme";
-                path = with pkgs; [colorctl];
-                script = "colorctl build ${optionalString (cfg.reload-command != null) "--reload --reload-command ${cfg.reload-command}"} ${name}";
-                wantedBy = ["sleep.target"];
-              })
-          )
-          cfg.settings;
-
-        systemd.user.timers =
-          mapAttrs' (
-            name: cfg:
-              nameValuePair "apply-${name}-theme" (mkIf cfg.enable {
-                description = "Re-apply ${name} theme";
-                partOf = ["apply-${name}-theme.service"];
-                wantedBy = ["timers.target"];
-                timerConfig.OnCalendar = "hourly";
-              })
-          )
-          cfg.settings;
-      })
-    ]
-  );
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [colorctl];
+  };
 }
