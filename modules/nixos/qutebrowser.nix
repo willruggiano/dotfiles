52190f3eb53d409d1f7184cf9abe5d8d6ee462e4
Will Ruggiano <wmruggiano@gmail.com>
2022-12-01 12:55:43 -0700

    fix(qute): temporarily disable spellcheck dictionaries

--- modules/nixos/qutebrowser.nix
+++ modules/nixos/qutebrowser.nix
@@ -28,12 +28,13 @@ in {
     ];
 
     # Install language dictionaries for spellcheck backends
-    system.userActivationScripts.qutebrowserInstallDicts = concatStringsSep "\\\n" (map
-    (lang: ''
-      if ! find "$HOME/.config/qutebrowser/qtwebengine_dictionaries" -type d -maxdepth 1 -name "${lang}*" 2>/dev/null | grep -q .; then
-        ${pkgs.python3}/bin/python ${pkgs.qutebrowser}/share/qutebrowser/scripts/dictcli.py install ${lang}
-      fi
-    '')
-    cfg.dicts);
+    # TODO: Re-enable this when a new release is created; see: https://github.com/qutebrowser/qutebrowser/issues/7481
+    # system.userActivationScripts.qutebrowserInstallDicts = concatStringsSep "\\\n" (map
+    # (lang: ''
+    #   if ! find "$HOME/.config/qutebrowser/qtwebengine_dictionaries" -type d -maxdepth 1 -name "${lang}*" 2>/dev/null | grep -q .; then
+    #     ${pkgs.python3}/bin/python ${pkgs.qutebrowser}/share/qutebrowser/scripts/dictcli.py install ${lang}
+    #   fi
+    # '')
+    # cfg.dicts);
   };
 }
