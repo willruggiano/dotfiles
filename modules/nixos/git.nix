b5c8fc27df8c83bd7af56fb1ea3e9a54f7696e38
Will Ruggiano <wmruggiano@gmail.com>
2023-02-03 02:02:59 -0700

    feat(flavours): lazygit

--- modules/nixos/git.nix
+++ modules/nixos/git.nix
@@ -1,5 +1,8 @@
 {pkgs, ...}: {
   config = {
-    programs.colorctl.settings.lazygit.enable = true;
+    programs.flavours.items.lazygit = {
+      file = "~/.config/lazygit/theme.yml";
+      template = "custom/lazygit";
+    };
   };
 }
