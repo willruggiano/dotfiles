aef481ed2ef912862b5882f05836fad0eefbb743
Will Ruggiano <wmruggiano@gmail.com>
2023-03-14 12:17:08 -0600

    fix: spotifyd

--- modules/nixos/spotify.nix
+++ modules/nixos/spotify.nix
@@ -12,12 +12,17 @@ with lib; let
       if config.hardware.pulseaudio.enable
       then "pulseaudio"
       else "alsa";
+    device = "spotifyd@${config.networking.hostName}";
   in
     pkgs.writeText "spotifyd.conf" ''
       [global]
       username = "1211559862"
       password_cmd = "cat ${config.age.secrets.spotify.path}"
+
       backend = "${backend}"
+
+      device_name = "${device}"
+      device_type = "computer"
     '';
 in {
   options.programs.spotify = {
@@ -29,6 +34,8 @@ in {
 
     systemd.user.services.spotifyd = {
       description = "spotifyd, a Spotify playing daemon";
+      after = ["network-online.target" "sound.target"];
+      wants = ["network-online.target" "sound.target"];
       wantedBy = ["default.target"];
       environment.SHELL = "/bin/sh";
       serviceConfig = {
