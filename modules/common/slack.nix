27376392475f1384c8f776c86c882367377fc6d8
Will Ruggiano <wmruggiano@gmail.com>
2023-03-20 10:20:26 -0600

    fix: slack

--- modules/common/slack.nix
+++ modules/common/slack.nix
@@ -6,13 +6,6 @@
 }:
 with lib; let
   cfg = config.programs.slack;
-  configfile = pkgs.writeTextFile {
-    name = "slack-term-config";
-    text = builtins.toJSON {
-      notify = "mention";
-      emoji = true;
-    };
-  };
 in {
   options.programs.slack = {
     enable = mkEnableOption "Enable slack";
@@ -20,17 +13,7 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = [
-      (pkgs.runCommand ".slack-term-wrapped" {
-        buildInputs = with pkgs; [makeWrapper];
-      } ''
-        mkdir $out
-        ln -s ${pkgs.slack-term}/* $out
-        rm $out/bin
-        mkdir $out/bin
-        makeWrapper ${pkgs.slack-term}/bin/slack-term $out/bin/slack \
-          --add-flags "-config ${configfile}"                        \
-          --add-flags "-token \"\$(cat ${config.age.secrets.slack.path})\""
-      '')
+      pkgs.slack
     ];
   };
 }
