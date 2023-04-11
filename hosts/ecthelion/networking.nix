33bec2580f8fac1d214c7ec7490232a572fb82b0
Will Ruggiano <wmruggiano@gmail.com>
2023-04-10 21:28:45 -0600

    feat(mothership): declarative wireless networks

--- hosts/ecthelion/networking.nix
+++ hosts/ecthelion/networking.nix
@@ -1,9 +1,5 @@
 {config, ...}: {
   networking.hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
-  networking.networkmanager.enable = true;
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.enp6s0.useDHCP = true;
 }
--- hosts/mothership/networking.nix
+++ hosts/mothership/networking.nix
@@ -1,10 +1,18 @@
 {config, ...}: {
   networking.hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
-  networking.networkmanager.enable = true;
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
-  # networking.interfaces.tun0.useDHCP = true;
   networking.interfaces.wlp2s0.useDHCP = true;
+
+  networking.wireless = {
+    enable = true;
+    environmentFile = config.age.secrets."networking.env".path;
+    networks = {
+      NETGEAR39 = {
+        pskRaw = "@PSK_NETGEAR_39@";
+      };
+      NETGEAR39-5G = {
+        pskRaw = "@PSK_NETGEAR_39_5G@";
+      };
+    };
+  };
 }
