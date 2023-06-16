b5421edd832f0695383dfe144d630625bf154f26
Will Ruggiano <wmruggiano@gmail.com>
2023-06-16 12:46:29 -0600

    fix(mothership): enable keyd only for specific keyboards

--- hosts/mothership/keyboard-layout.nix
+++ hosts/mothership/keyboard-layout.nix
@@ -2,7 +2,10 @@
   config = {
     services.keyd = {
       enable = true;
-      ids = ["*"];
+      ids = [
+        "0001:0001" # builtin
+        "045e:0917" # Surface Keyboard
+      ];
     };
   };
 }
