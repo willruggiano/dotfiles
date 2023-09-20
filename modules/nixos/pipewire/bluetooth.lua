5b3652512151f9523d759295677a2e14104ca90f
Will Ruggiano <wmruggiano@gmail.com>
2023-09-20 13:21:22 -0600

    fix: bluetooth?

--- modules/nixos/pipewire/bluetooth.lua
+++ modules/nixos/pipewire/bluetooth.lua
@@ -3,17 +3,4 @@ bluez_monitor.properties = {
   ["bluez5.enable-sbc-xq"] = true,
   ["bluez5.enable-hw-volume"] = true,
   ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]",
-
-  rules = {
-    {
-      matches = {
-        {
-          { "device.name", "matches", "bluez_card.*" },
-        },
-      },
-      apply_properties = {
-        ["bluez5.auto-connect"] = "[ hfp_hf hsp_hs a2dp_sink ]",
-      },
-    },
-  },
 }
