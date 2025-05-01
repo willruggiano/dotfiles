9b7ab6e11480bc94bc263a3944ddf94e2749b69a
Will Ruggiano <mix.spend.dough@usecloaked.com>
2025-04-30 21:16:32 -0700

    feat: iwd

--- hosts/mothership/networking.nix
+++ hosts/mothership/networking.nix
@@ -1,6 +1,6 @@
 {config, ...}: {
-  networking.hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
-  networking.useDHCP = false;
-  networking.interfaces.wlp2s0.useDHCP = true;
-  networking.networkmanager.enable = true;
+  networking = {
+    hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
+    wireless.iwd.enable = true;
+  };
 }
