2d0e3b8bb6e838b7b752c00119868fe2c9da7823
Will Ruggiano <mix.spend.dough@usecloaked.com>
2025-06-06 15:55:02 -0700

    feat: until.fish

--- modules/nixos/fish/functions/magic-enter.fish
+++ modules/nixos/fish/functions/magic-enter.fish
@@ -1,8 +1,8 @@
 function magic-enter
-    set -l cmd (commandline)
-    if test -z "$cmd"
-        commandline -r (magic-enter-cmd)
-        commandline -f suppress-autosuggestion
-    end
-    commandline -f execute
+  set -l cmd (commandline)
+  if test -z "$cmd"
+    commandline -r (magic-enter-cmd)
+    commandline -f suppress-autosuggestion
+  end
+  commandline -f execute
 end
---
+++ modules/nixos/fish/functions/until.fish
@@ -0,0 +1,13 @@
+function until --description "Until you succeed, I say!"
+  set -l i 1
+  while ! $argv
+    set -l i (math "$i + 1")
+    sleep 1
+  end
+  if test $status -eq 0
+    printf "\n$argv succeeded after $i iterations\n"
+    if test -z "$SSH_CONNECTION"
+      notify-send "$argv succeeded after $i iterations"
+    end
+  end
+end
