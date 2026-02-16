5ac730466819b2f140e0b4ab680f6c675e430869
Will Ruggiano <mix.spend.dough@usecloaked.com>
2026-02-15 18:47:47 -0800

    feat: impala

--- hosts/mothership/networking.nix
+++ hosts/mothership/networking.nix
@@ -1,4 +1,9 @@
-{config, ...}: {
+{
+  config,
+  pkgs',
+  ...
+}: {
+  environment.systemPackages = with pkgs'; [impala];
   networking = {
     hostId = builtins.substring 0 8 (builtins.hashString "md5" config.networking.hostName);
     wireless.iwd.enable = true;
