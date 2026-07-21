8f21b2b841a19dbf16823db1afa3ac0dcb84a21c
Will Ruggiano <mix.spend.dough@usecloaked.com>
2026-07-21 01:16:27 -0700

    chore: remove unused api keys

--- hosts/ecthelion/secrets/default.nix
+++ hosts/ecthelion/secrets/default.nix
@@ -2,9 +2,6 @@ let
   host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN8WeTBtGhbaMf1TZ/aNFyJuEZpUo/N4ZA7F+5PIzjG1 root@ecthelion";
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEWXJHDkOTwqq+3W5JgBxGWyDNlhxVcQB/2lwBRwg8/f bombadil@ecthelion";
 in {
-  "anthropic.age".publicKeys = [host user];
   "bombadil@ecthelion.age".publicKeys = [host user];
-  "gemini.age".publicKeys = [host user];
-  "openai.age".publicKeys = [host user];
   "willruggiano@github.age".publicKeys = [host user];
 }
--- hosts/mothership/secrets/default.nix
+++ hosts/mothership/secrets/default.nix
@@ -2,9 +2,6 @@ let
   host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXEBxFhZ88up2XVDN3SOMjuFNczXJz4ZWx1NJuq42Uc root@mothership";
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERAQpJ3mjcz+b2Y+Wf598wURIrGU710Sr91HCcwSiXS bombadil@mothership";
 in {
-  "anthropic.age".publicKeys = [host user];
   "bombadil@mothership.age".publicKeys = [host user];
-  "gemini.age".publicKeys = [host user];
-  "openai.age".publicKeys = [host user];
   "willruggiano@github.age".publicKeys = [host user];
 }
