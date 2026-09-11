484fbc428b40ef3028a7537cd9daa5194522df54
Will Ruggiano <mix.spend.dough@usecloaked.com>
2026-09-10 20:50:29 -0700

    fix: disable keybase-gui

--- modules/nixos/keybase.nix
+++ modules/nixos/keybase.nix
@@ -1,13 +1,13 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 with lib; let
   cfg = config.services.kbfs;
 in {
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [keybase-gui];
+    # insecure?
+    # environment.systemPackages = with pkgs; [keybase-gui];
   };
 }
